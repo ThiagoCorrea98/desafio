5a847cc658f9ff1d61a0e96bff47f9733a62edd7
gempe <thiago.correa@startse.com>
2021-12-10 16:50:34 -0300

    Feat/creat component SideBar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,40 @@
-export function SideBar() {
+import { useEffect, useState } from "react";
+import { GenreResponse } from "../model/movies";
+import { api } from "../services/api";
+import { Button } from "./Button";
+import '../styles/content.scss';
+
+interface SideBarProps {
+  onTransactionMovie: (id: number) => void;
+  selectedGenreId: number;
+}
+
+export function SideBar({ onTransactionMovie, selectedGenreId }: SideBarProps) {
   // Complete aqui
+  const [genres, setGenres] = useState<GenreResponse[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponse[]>("genres").then((response) => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onTransactionMovie(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
